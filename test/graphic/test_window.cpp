39fc13b25fbdd9f9e5f059cf8f5c69e9e623f096
Cora <17833654450@163.com>
2026-07-29 13:25:45 +0800

    fix error return value check

--- test/graphic/test_window.cpp
+++ test/graphic/test_window.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
 
 #pragma region Init
     // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
         std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << '\n';
         return 1;
     }
