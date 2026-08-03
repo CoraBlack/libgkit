96317b9902a72aaf492466b6432e4325156b1b79
Cora <17833654450@163.com>
2026-08-03 17:08:31 +0800

    add test utils

---
+++ test/test_utils.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <functional>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <format>
+#include <cstdlib>
+
+namespace gkit::test {
+    class TestRunner {
+        std::vector<std::function<bool()>> test_funcs;
+        
+    public:
+        TestRunner()  = default;
+        ~TestRunner() = default;
+
+        auto add_test_func(std::function<bool()>&& func) {
+            this->test_funcs.emplace_back(std::move(func));
+        }
+
+        auto run() -> void {
+            auto res = true;
+            auto fail_count = 0;
+            auto test_count = this->test_funcs.size();
+
+            for (auto&& func : test_funcs) {
+                if (!func()) {
+                    fail_count += 1;
+                    res = false;
+                }
+            }
+
+            if (res) {
+                std::cout << std::format("All tests(total {}) passed!", test_count) << '\n';
+            } else {
+                std::cout << std::format("Some tests(total {}) failed!", fail_count) << '\n';
+            }
+
+            return;
+        }
+    };
+
+    auto assert(bool cond, std::string failed_msg) -> void { // NOLINT(performance-unnecessary-value-param)
+        if (!cond) {
+            std::cout << failed_msg << '\n';
+            std::abort();
+        }
+    }
+
+    template<class... Args>
+    auto logln(const char* fmt, Args&&... args) -> void {
+        std::cout << std::format(fmt, std::forward<Args>(args)...) << '\n';
+    }
+} // namespace gkit::test
